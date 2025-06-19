305ee96018adf7f5773fc2c5699b77633625b352
mumulin1117 <mumulin1117@163.com>
2025-06-19 14:30:34 +0800

    Initial commit

---
+++ Kreyuosen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kreyuosen
+//
+//  Created by mumu on 2025/6/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
